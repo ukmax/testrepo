24336cbda9e3ec7e3db1a48283347cb5f38a6393
Maxim <max@max.local>
2023-05-03 01:29:59 +0300

    first commit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+# комментарий в ветке branch_one
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
