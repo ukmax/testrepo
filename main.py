04b5822dbea4b8d4a35ebeafbcd429256293b397
Maxim <max@max.local>
2023-05-03 01:35:00 +0300

    комментарий 2 в ветке main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 # комментарий в ветке main
+# комментарий 2 в ветке main
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
